c6bf0d534e792aaccaed59c939a05cc42723d4fb
johnvincentio <john@johnvincent.io>
2021-03-07 15:46:06 -0500

    add

--- template/react-todo-list-episodes/src/index.tsx
+++ template/react-todo-list-episodes/src/index.tsx
@@ -1,10 +1,35 @@
+//
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+// import React from 'react';
+import * as React from 'react';
 
-import { App } from "./components/App";
-import "./styles/style.scss";
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-const ROOT = document.querySelector(".container");
+// import { Router, RouteComponentProps } from '@reach/router';
 
-ReactDOM.render(<App />, ROOT);
+import { StoreProvider } from './Store';
+
+import HomePage from './HomePage';
+import FavPage from './FavPage';
+
+import App from './App';
+
+// const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps) => props.pageComponent;
+
+document.addEventListener('DOMContentLoaded', () => {
+	ReactDOM.render(
+		<StoreProvider>
+		<Router>
+			<App path="/">
+				<Route exact path="/favs" render={props => <FavPage />} />
+				<Route exact path="/" render={props => <HomePage />} />
+				{/* <RouterPage pageComponent={<HomePage />} path="/" /> */}
+				{/* <RouterPage pageComponent={<FavPage />} path="/favs" /> */}
+				<Redirect to="/" />
+			</App>
+		</Router>
+	</StoreProvider>,
+		document.getElementById('root')
+	);
+});
