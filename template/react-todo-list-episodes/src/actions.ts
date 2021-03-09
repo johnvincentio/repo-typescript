928946535b0a1e870e4d71b0c63623dc078b47bc
johnvincentio <john@johnvincent.io>
2021-03-08 20:04:10 -0500

    add

--- template/react-todo-list-episodes/src/actions.ts
+++ template/react-todo-list-episodes/src/actions.ts
@@ -3,24 +3,25 @@
 import { IAction, IEpisode, IState } from './interfaces';
 
 export const fetchDataAction = async (dispatch: any) => {
-  const URL =
+	const URL =
     'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
-  const data = await fetch(URL);
-  const dataJSON = await data.json();
-  return dispatch({
-    type: 'FETCH_DATA',
-    payload: dataJSON._embedded.episodes,
-  });
+	const data = await fetch(URL);
+	const dataJSON = await data.json();
+	return dispatch({
+		type: 'FETCH_DATA',
+		// eslint-disable-next-line no-underscore-dangle
+		payload: dataJSON._embedded.episodes,
+	});
 };
 
 export const toggleFavAction = (
-  state: IState,
-  dispatch: any,
-  episode: IEpisode
+	state: IState,
+	dispatch: any,
+	episode: IEpisode
 ): IAction => {
-  const dispatchObj = {
-    type: 'ADD_FAV',
-    payload: episode,
-  };
-  return dispatch(dispatchObj);
+	const dispatchObj = {
+		type: 'ADD_FAV',
+		payload: episode,
+	};
+	return dispatch(dispatchObj);
 };
--- template/react-todo-list-episodes/src/index.tsx
+++ template/react-todo-list-episodes/src/index.tsx
@@ -1,12 +1,11 @@
-// import React from 'react';
+
 import * as React from 'react';
 
 import * as ReactDOM from 'react-dom';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+	BrowserRouter as Router,
+	Route,
+	Redirect,
 } from 'react-router-dom';
 
 // import { Router, RouteComponentProps } from '@reach/router';
@@ -23,18 +22,18 @@ import './styles.scss';
 // const RouterPage = (props: { pageComponent: JSX.Element } & RouteComponentProps) => props.pageComponent;
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <StoreProvider>
-      <Router>
-        <App path="/">
-          <Route exact path="/favs" render={(props) => <FavPage />} />
-          <Route exact path="/" render={(props) => <HomePage />} />
-          {/* <RouterPage pageComponent={<HomePage />} path="/" /> */}
-          {/* <RouterPage pageComponent={<FavPage />} path="/favs" /> */}
-          <Redirect to="/" />
-        </App>
-      </Router>
-    </StoreProvider>,
-    document.getElementById('root')
-  );
+	ReactDOM.render(
+		<StoreProvider>
+			<Router>
+				<App path="/">
+					<Route exact path="/favs" render={(props) => <FavPage />} />
+					<Route exact path="/" render={(props) => <HomePage />} />
+					{/* <RouterPage pageComponent={<HomePage />} path="/" /> */}
+					{/* <RouterPage pageComponent={<FavPage />} path="/favs" /> */}
+					<Redirect to="/" />
+				</App>
+			</Router>
+		</StoreProvider>,
+		document.getElementById('root')
+	);
 });
