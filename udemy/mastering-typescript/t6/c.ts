c5092971f087d769526183e16bf039e365938e00
johnvincentio <john@johnvincent.io>
2022-07-07 13:45:36 -0400

    add

--- udemy/mastering-typescript/t6/c.ts
+++ udemy/mastering-typescript/t6/c.ts
@@ -1,3 +1,26 @@
 
 let age: number | string = 21;
 
+function abc(a: number): void {
+
+}
+
+function abc(a: string): void {
+    
+}
+
+const stuff: (number | string)[] = [1,2,`qw`]
+
+const ss: (Point | Loc)[] = [];
+
+const zero: 0 = 0;
+
+let mood: "happy" | "sad" = "happy";
+mood = "sad";
+mood = "xyz";
+
+type DayOfWeek = "Mon" | "Tue" | "wed" | "thu" | "fri" | "sat"| "sun";
+
+let abc: DayOfWeek = "xyz";
+
+
