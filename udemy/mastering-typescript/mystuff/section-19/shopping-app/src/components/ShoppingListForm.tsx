8f12ab52f91f08d9186e24d03966112c312c39d5
johnvincentio <john@johnvincent.io>
2022-07-15 18:44:18 -0400

    clean

--- udemy/mastering-typescript/mystuff/section-19/shopping-app/src/components/ShoppingListForm.tsx
+++ udemy/mastering-typescript/mystuff/section-19/shopping-app/src/components/ShoppingListForm.tsx
@@ -8,6 +8,7 @@ interface ShoppingListFormProps {
 function ShoppingListForm({ onAddItem }: ShoppingListFormProps): JSX.Element {
   const productInputRef = useRef<HTMLInputElement>(null);
   const quantityInputRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const newProduct = productInputRef.current!.value;
@@ -16,6 +17,7 @@ function ShoppingListForm({ onAddItem }: ShoppingListFormProps): JSX.Element {
     productInputRef.current!.value = "";
     quantityInputRef.current!.value = "1";
   };
+  
   return (
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Product Name" ref={productInputRef} />
